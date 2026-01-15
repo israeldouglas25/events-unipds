2f15a8551ff2ce479e9cb809e2eb82fc6c70bf6c
israel.paixao <israeldouglas25@gmail.com>
2026-01-15 17:34:19 -0300

    criando a classe conference

--- src/main/java/br/com/israel/events/domain/Conference.java
+++ src/main/java/br/com/israel/events/domain/Conference.java
@@ -1,4 +1,22 @@
 package br.com.israel.events.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Conference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idConference;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String adress;
+
 }
