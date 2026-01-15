9cc01eb07c63259b4c270720fa7c83e0f4a06ce8
israel.paixao <israeldouglas25@gmail.com>
2026-01-15 17:30:44 -0300

    criando a classe user

--- src/main/java/br/com/israel/events/domain/User.java
+++ src/main/java/br/com/israel/events/domain/User.java
@@ -1,5 +1,20 @@
 package br.com.israel.events.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
 
 }
