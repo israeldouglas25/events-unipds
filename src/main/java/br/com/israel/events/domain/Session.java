944d5dee9bfb525996ec0528f8a3856521878719
israel.paixao <israeldouglas25@gmail.com>
2026-01-15 18:04:17 -0300

    criando a classe session

--- src/main/java/br/com/israel/events/domain/Session.java
+++ src/main/java/br/com/israel/events/domain/Session.java
@@ -1,4 +1,31 @@
 package br.com.israel.events.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "tb_session")
+@Getter
+@Setter
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSession;
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @ManyToOne
+    @JoinColumn(name = "tb_conference_id_conference")
+    private Conference conference;
+
 }
